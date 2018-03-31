10286bf8d06c53fc145282788298cc92013f5577
Sdinapoli <sdinapoli86@hotmail.com>
2018-03-31 22:56:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   token: process.env.BOT_TOKEN,
    autorun: true
 });
 bot.on('ready', function (evt) {
